119ae4763201432c24076b9452e472f848a80e5c
IgorKulduaev <and.sobol39@mail.ru>
2025-09-27 00:05:39 +0200

    Update Program.cs

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,14 +1,7 @@
-﻿
-﻿
-//Console.Write("введите t");
-//double t = double.Parse(Console.ReadLine());
-//Console.Write("введите b");
-//double b = double.Parse(Console.ReadLine());
-
-
 //Лабораторная работа 1
 //Арифметичейские операции
 //Вариант:17
+
 //Console.Write("введите x");
 //double x = double.Parse(Console.ReadLine());
 //Console.Write("введите a");
@@ -17,17 +10,18 @@
 //double b = double.Parse(Console.ReadLine());
 //Console.Write("введите y");
 //double y = double.Parse(Console.ReadLine());
-//Console.WriteLine($"T={(Math.Sqrt(x+b-a)+(Math.Log(y)))/Math.Atan(b+a)}");
+//Console.WriteLine($"T={(Math.Sqrt(x + b - a) + (Math.Log(y))) / Math.Atan(b + a)}");
 
 //Лабораторная работа 2
 //Программирование линейных логарифмов
 //Вариант:17
-x=0.9
-t=2
-Console.WriteLine($"{b=Math.Log(2)*Math.Abs(x)}");
-Console.WriteLine($"{a=tx+Math.Abs(Math.Sqrt(b))}");
-Console.WriteLine($"{y=Math.Cos(3)*(a+b(3))}"); C: 
 
-//Лабораторная работа 3
-//Целочисленная арифметика
-//Вариант:
+//Console.Write("введите x");
+//double x = double.Parse(Console.ReadLine());
+//double t = 2;
+//double b = Math.Pow(Math.Log10(Math.Abs(x)), 2);
+//double a = t * x + Math.Abs(Math.Sqrt(b));
+//double y = Math.Pow(Math.Cos(a + Math.Pow(b, 3)), 3);
+//Console.WriteLine($"a = {a}");
+//Console.WriteLine($"b = {b}");
+//Console.WriteLine($"y = {y}");
