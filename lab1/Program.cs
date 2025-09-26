056464358b24ba1ff6ef1a950f51d0239fb29ad9
IgorKulduaev <and.sobol39@mail.ru>
2025-09-26 23:12:30 +0200

    Update Program.cs

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+﻿
 //Console.Write("введите t");
 //double t = double.Parse(Console.ReadLine());
 //Console.Write("введите b");
@@ -8,19 +9,24 @@
 //Лабораторная работа 1
 //Арифметичейские операции
 //Вариант:17
-Console.Write("введите x");
-double x = double.Parse(Console.ReadLine());
-Console.Write("введите a");
-double a = double.Parse(Console.ReadLine());
-Console.Write("введите b");
-double b = double.Parse(Console.ReadLine());
-Console.Write("введите y");
-double y = double.Parse(Console.ReadLine());
-Console.WriteLine($"T={(Math.Sqrt(x+b-a)+(Math.Log(y)))/Math.Atan(b+a)}");
+//Console.Write("введите x");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("введите a");
+//double a = double.Parse(Console.ReadLine());
+//Console.Write("введите b");
+//double b = double.Parse(Console.ReadLine());
+//Console.Write("введите y");
+//double y = double.Parse(Console.ReadLine());
+//Console.WriteLine($"T={(Math.Sqrt(x+b-a)+(Math.Log(y)))/Math.Atan(b+a)}");
 
 //Лабораторная работа 2
 //Программирование линейных логарифмов
-//Вариант:
+//Вариант:17
+x=0.9
+t=2
+Console.WriteLine($"{b=Math.Log(2)*Math.Abs(x)}");
+Console.WriteLine($"{a=tx+Math.Abs(Math.Sqrt(b))}");
+Console.WriteLine($"{y=Math.Cos(3)*(a+b(3))}"); C: 
 
 //Лабораторная работа 3
 //Целочисленная арифметика
