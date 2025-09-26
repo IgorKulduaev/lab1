d690ddbb8e3dc96bb2760f491faa30c68d363c78
IgorKulduaev <and.sobol39@mail.ru>
2025-09-26 23:11:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+//Console.Write("введите t");
+//double t = double.Parse(Console.ReadLine());
+//Console.Write("введите b");
+//double b = double.Parse(Console.ReadLine());
+
+
+//Лабораторная работа 1
+//Арифметичейские операции
+//Вариант:17
+//Console.Write("введите x");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("введите a");
+//double a = double.Parse(Console.ReadLine());
+//Console.Write("введите b");
+//double b = double.Parse(Console.ReadLine());
+//Console.Write("введите y");
+//double y = double.Parse(Console.ReadLine());
+//Console.WriteLine($"T={(Math.Sqrt(x+b-a)+(Math.Log(y)))/Math.Atan(b+a)}");
+
+//Лабораторная работа 2
+//Программирование линейных логарифмов
+//Вариант:17
+x=0.9
+t=2
+Console.WriteLine($"{b=Math.Log(2)*Math.Abs(x)}");
+Console.WriteLine($"{a=tx+Math.Abs(Math.Sqrt(b))}");
+Console.WriteLine($"{y=Math.Cos(3)*(a+b(3))}"); C: 
+
+//Лабораторная работа 3
+//Целочисленная арифметика
+//Вариант:
