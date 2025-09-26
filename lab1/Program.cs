058454c5b7a9797d490aab82604c93cf6a1b2a66
Kulduaev <Kulduaev@209-CH-01>
2025-09-26 14:41:10 +0200

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Console.Write("введите t");
+//double t = double.Parse(Console.ReadLine());
+//Console.Write("введите b");
+//double b = double.Parse(Console.ReadLine());
+
+
+//Лабораторная работа 1
+//Арифметичейские операции
+//Вариант:17
+Console.Write("введите x");
+double x = double.Parse(Console.ReadLine());
+Console.Write("введите a");
+double a = double.Parse(Console.ReadLine());
+Console.Write("введите b");
+double b = double.Parse(Console.ReadLine());
+Console.Write("введите y");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine($"T={(Math.Sqrt(x+b-a)+(Math.Log(y)))/Math.Atan(b+a)}");
+
+//Лабораторная работа 2
+//Программирование линейных логарифмов
+//Вариант:
+
+//Лабораторная работа 3
+//Целочисленная арифметика
+//Вариант:
